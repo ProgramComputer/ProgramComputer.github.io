ebc1b9de5fba431365b62ccd9fcb9b4224eaa11f
ProgramComputer <22284856+ProgramComputer@users.noreply.github.com>
2025-05-24 11:26:10 -0500

    Update default theme in gitprofile.config.ts from 'Winter' to 'light'

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -144,7 +144,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'Winter',
+    defaultTheme: 'light',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
